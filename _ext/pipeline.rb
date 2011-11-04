eda6ad1e78870a1371d78aef5eeba765fb0b8d0d
Trustin Lee <trustin@gmail.com>
2011-11-04 02:15:18 -0700

    Added IntenseDebate

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,6 +2,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/blog' ) 
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5 )
   extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/index.atom')
+  extension Awestruct::Extensions::IntenseDebate.new
 
   helper Awestruct::Extensions::GoogleAnalytics
 end
