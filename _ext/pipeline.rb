7086db149c9310d7e11703125e75a4585804b28b
Trustin Lee <trustin@gmail.com>
2011-11-03 19:36:11 -0700

    Added blog and inserted Google Analytics tag

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,8 @@
-
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new( '/blog' ) 
+  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5 )
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/index.atom')
+
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
---
+++ blog/2000-01-01-announcing-new-web-site.md
@@ -0,0 +1,8 @@
+---
+title: 'Announcing New Web Site'
+author: Trustin Lee
+layout: blog
+---
+
+Coming soon. Stay tuned!
+
