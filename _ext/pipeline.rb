2f9aa2c3124043376f92eed0a470f53ee01912d0
Trustin Lee <trustin@gmail.com>
2011-12-07 13:46:54 +0900

    Use Disqus instead of IntenseDebate / New post

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,8 +1,10 @@
+require 'disqus'
+
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/blog' ) 
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5 )
   extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/index.atom')
-  extension Awestruct::Extensions::IntenseDebate.new
+  extension Awestruct::Extensions::Disqus.new
 
   helper Awestruct::Extensions::GoogleAnalytics
 end
