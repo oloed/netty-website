b8edfb8150f5a08c6ce98d49367f888ae7428e03
Trustin Lee <trustin@gmail.com>
2012-03-03 07:49:27 -0800

    Make sure all working copies are clean

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -19,9 +19,21 @@ if [[ ! -d "$DST/.git" ]]; then
 fi
 
 set -e
+
+# Make sure netty-website is clean
+pushd "$SRC/.."
+GIT_STATUS="$(git status 2> /dev/null)"
+if [[ ! ${GIT_STATUS} =~ (working directory clean) ]]; then
+  echo "Working directory not clean: $PWD"
+  exit 1
+fi
+popd
+
+# Make sure netty.github.com is clean
 pushd "$DST"
+
 if ! git remote -v | grep -qF 'git@github.com:netty/netty.github.com.git'; then
-  echo "Not a netty.github.com repository: $DST"
+  echo "Not a netty.github.com repository: $PWD"
   exit 1
 fi
 
@@ -33,19 +45,24 @@ fi
 
 popd
 
+# Generate the web site
 rm -fr "$SRC"
-
 pushd "$BIN/.."
 awestruct -g
 popd
 
+# Pull the latest changes in netty.github.com
 pushd "$DST"
 git pull --ff-only
 popd
 
+# Inject Google Analytics JavaScript in all generated HTMLs
 "$BIN/inject-google-analytics.sh"
+
+# Copy the generated web site to netty.github.com
 rsync -a "$SRC/" "$DST"
 
+# Publish the generated web site
 cd "$DST"
 git add .
 git commit -m "Deploy site '$(git log -1 --format=format:%h)' on $(date)"
